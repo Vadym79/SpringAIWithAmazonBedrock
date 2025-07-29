9c9b87c9a96a89802cd0ab694ccbe779bf295c21
Vadym Kazulkin <v.kazulkin@iplabs.de>
2025-07-29 20:29:35 +0200

    rewritten to use stream

--- src/main/java/dev/vkazulkin/tool/conference/ConferenceSearchTool.java
+++ src/main/java/dev/vkazulkin/tool/conference/ConferenceSearchTool.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.ai.tool.annotation.ToolParam;
@@ -36,24 +37,18 @@ public class ConferenceSearchTool {
 		System.out.println("search topic "+topic);
 		System.out.println("earliest start date "+earliestStartDate);
 		System.out.println("latest start date "+latestStartDate);
-		var conferenceByTopic= new HashSet<Conference>();		
+		Set<Conference> foundConferences;		
 		try (InputStream inputStream = TypeReference.class.getResourceAsStream("/conferences.json")) {
 			var conferences= this.objectMapper.readValue(inputStream, Conferences.class);
-			for(Conference conference: conferences.conferences()) {
-				LocalDate startDate= conference.startDate();
-				if(startDate.isAfter(earliestStartDate) && startDate.isBefore(latestStartDate)) {
-					for(String confTopic: conference.topics()) {		
-						if(confTopic.contains(topic)) {
-							   conferenceByTopic.add(conference);
-						}
-					}
-			    }
-			}
+			foundConferences= conferences.conferences().stream()
+			.filter(c -> c.topics().contains(topic))
+		    .filter(c -> c.startDate().isAfter(earliestStartDate) && c.startDate().isBefore(latestStartDate))
+		    .collect(Collectors.toSet());
 		} 
 		catch(IOException ex) {
 			throw new RuntimeException("can't read conferences");
 		}
-		System.out.println("return list of conferences: "+conferenceByTopic);
-		return conferenceByTopic;
+		System.out.println("return list of conferences: "+foundConferences);
+		return foundConferences;
 	  }
 }
