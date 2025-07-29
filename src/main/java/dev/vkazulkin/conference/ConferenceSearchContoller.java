59f97e470b618818dbd0da205c96970a10a5ea18
Vadym Kazulkin <v.kazulkin@iplabs.de>
2025-07-29 22:30:49 +0200

    added chat memory for the conference search tool

--- src/main/java/dev/vkazulkin/conference/ConferenceSearchContoller.java
+++ src/main/java/dev/vkazulkin/conference/ConferenceSearchContoller.java
@@ -1,6 +1,8 @@
 package dev.vkazulkin.conference;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.model.tool.ToolCallingChatOptions;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,20 +19,14 @@ public class ConferenceSearchContoller {
     private final ZonedDateTimeTool zonedDateTimeTool;
     private final ConferenceSearchTool conferenceSearchTool;
     
-    private static final String USER_PROMPT= """
-			
-			1. Provide me with the best suggestions to apply for the talk for the {topic} conferences.
-			2. The provided conference start date attribute should be within the next {number_of_months} months. 
-			3. Please include the following conference info in the response only: name, topics, homepage, start and end date, city and call for papers link
-			""";
-    
     private static final String SYSTEM_PROMPT="""
     		You are only able to answer questions about upcoming technical conferences. 
     		If the provided search term {topic} is not a technical term or is not connected to the conference for the software development, 
     		please respond in the friendly manner that you're not able to provide this information.
     		""";
     
-    public ConferenceSearchContoller(ChatClient.Builder builder, ZonedDateTimeTool zonedDateTimeTool, ConferenceSearchTool conferenceSearchTool) {
+    public ConferenceSearchContoller(ChatClient.Builder builder, ChatMemory chatMemory, 
+    		ZonedDateTimeTool zonedDateTimeTool, ConferenceSearchTool conferenceSearchTool) {
 		var options = ToolCallingChatOptions.builder()
 				 .model("amazon.nova-lite-v1:0")
 				 // .model("amazon.nova-pro-v1:0")
@@ -38,6 +34,7 @@ public class ConferenceSearchContoller {
 				 .maxTokens(2000).build();
 		
 		this.chatClient = builder
+				.defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
 				.defaultOptions(options)
 				//.defaultSystem(SYSTEM_PROMPT)
 				.build();
@@ -45,13 +42,29 @@ public class ConferenceSearchContoller {
         this.conferenceSearchTool=conferenceSearchTool;
     }
 
-    @GetMapping("/conference-search")
-    public Flux<String> conferenceSearch(@RequestParam(value = "topic", defaultValue = "Java") String topic,
+    @GetMapping("/conference-search-by-topic")
+    public Flux<String> conferenceSearchbyTopic(@RequestParam(value = "topic", defaultValue = "Java") String topic,
     		@RequestParam(value = "number_of_months", defaultValue = "6") String numOfMonths) {
+    	
+    	final String USER_PROMPT= """
+    			
+    			1. Provide me with the best suggestions to apply for the talk for the {topic} conferences.
+    			2. The provided conference start date attribute should be within the next {number_of_months} months. 
+    			3. Please include the following conference info in the response only: name, topics, homepage, start and end date, city and call for papers link
+    			""";
         return this.chatClient.prompt()
-        		.system(s -> s.text(SYSTEM_PROMPT).param("topic", topic))
+        		 .system(s -> s.text(SYSTEM_PROMPT).param("topic", topic))
         		.user(u -> u.text(USER_PROMPT).param("topic", topic).param("number_of_months", numOfMonths))
                 .tools(this.zonedDateTimeTool, this.conferenceSearchTool)
+                .stream()
+                .content();
+    }
+    
+    @GetMapping("/conference-search")
+    public Flux<String> conferenceSearch(@RequestParam(value = "prompt") String prompt) {
+       return this.chatClient.prompt()
+        		.user(prompt)
+                .tools(this.conferenceSearchTool)
                 .stream()
                 .content();
     }
--- src/main/java/dev/vkazulkin/tool/conference/ConferenceSearchTool.java
+++ src/main/java/dev/vkazulkin/tool/conference/ConferenceSearchTool.java
@@ -3,7 +3,6 @@ package dev.vkazulkin.tool.conference;
 import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDate;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -29,7 +28,7 @@ public class ConferenceSearchTool {
 
 	}
 
-	@Tool(name="Conference_Search_Tool", description = "Search for the conference list for exactly one topic provided")
+	@Tool(name="Conference_Search_Tool_By_Topic", description = "Search for the conference list for exactly one topic provided")
     public Set<Conference> search(@ToolParam(description = "conference topic") String topic,
     		@ToolParam(description = " the conference earliest start date") LocalDate earliestStartDate,
     		@ToolParam(description = " the conference latest start date") LocalDate latestStartDate) {
@@ -38,17 +37,29 @@ public class ConferenceSearchTool {
 		System.out.println("earliest start date "+earliestStartDate);
 		System.out.println("latest start date "+latestStartDate);
 		Set<Conference> foundConferences;		
+		foundConferences= this.getAllConferences().conferences().stream()
+		.filter(c -> c.topics().contains(topic))
+		.filter(c -> c.startDate().isAfter(earliestStartDate) && c.startDate().isBefore(latestStartDate))
+		.collect(Collectors.toSet());
+		
+		System.out.println("return list of conferences: "+foundConferences);
+		return foundConferences;
+	  }
+	
+	
+	@Tool(name="All_Conference_Search_Tool", description = "Get the list of all conferences and answer questions about them")
+    public Set<Conference> searchAllConferences() {
+		System.out.println("Search for all conferences");
+		return this.getAllConferences().conferences();
+	  }
+
+	
+	private Conferences getAllConferences() {
 		try (InputStream inputStream = TypeReference.class.getResourceAsStream("/conferences.json")) {
-			var conferences= this.objectMapper.readValue(inputStream, Conferences.class);
-			foundConferences= conferences.conferences().stream()
-			.filter(c -> c.topics().contains(topic))
-		    .filter(c -> c.startDate().isAfter(earliestStartDate) && c.startDate().isBefore(latestStartDate))
-		    .collect(Collectors.toSet());
+			return this.objectMapper.readValue(inputStream, Conferences.class);
 		} 
 		catch(IOException ex) {
 			throw new RuntimeException("can't read conferences");
 		}
-		System.out.println("return list of conferences: "+foundConferences);
-		return foundConferences;
-	  }
+	}
 }
