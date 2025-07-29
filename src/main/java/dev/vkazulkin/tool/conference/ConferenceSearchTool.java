376cb6c11bfd8b04306d23e95945d92175c20263
Vadym Kazulkin <v.kazulkin@iplabs.de>
2025-07-29 19:46:36 +0200

    provided start and end conference dates as tool parameters

--- src/main/java/dev/vkazulkin/tool/conference/ConferenceSearchTool.java
+++ src/main/java/dev/vkazulkin/tool/conference/ConferenceSearchTool.java
@@ -2,6 +2,7 @@ package dev.vkazulkin.tool.conference;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,26 +19,35 @@ import dev.vkazulkin.conference.Conferences;
 
 @Component
 public class ConferenceSearchTool {
-	
+
 	private final ObjectMapper objectMapper;
-	
+
 	public ConferenceSearchTool(ObjectMapper objectMapper) {
-		this.objectMapper=objectMapper;
+		this.objectMapper = objectMapper;
 		this.objectMapper.registerModule(new JavaTimeModule());
 
 	}
-	
+
 	@Tool(name="Conference_Search_Tool", description = "Search for the conference list for exactly one topic provided")
-    public Set<Conference> search(@ToolParam(description = "Search for conference list for exactly one topic provided in the user's prompt. Only include topic as a search string") String topic) {
+    public Set<Conference> search(@ToolParam(description = "conference topic") String topic,
+    		@ToolParam(description = " the conference earliest start date") LocalDate earliestStartDate,
+    		@ToolParam(description = " the conference latest start date") LocalDate latestStartDate) {
     	
 		System.out.println("search topic "+topic);
+		System.out.println("earliest start date "+earliestStartDate);
+		System.out.println("latest start date "+latestStartDate);
 		var conferenceByTopic= new HashSet<Conference>();		
 		try (InputStream inputStream = TypeReference.class.getResourceAsStream("/conferences.json")) {
 			var conferences= this.objectMapper.readValue(inputStream, Conferences.class);
 			for(Conference conference: conferences.conferences()) {
-				if (conference.topics().contains(topic)) {
-					conferenceByTopic.add(conference);
-				}
+				LocalDate startDate= conference.startDate();
+				if(startDate.isAfter(earliestStartDate) && startDate.isBefore(latestStartDate)) {
+					for(String confTopic: conference.topics()) {		
+						if(confTopic.contains(topic)) {
+							   conferenceByTopic.add(conference);
+						}
+					}
+			    }
 			}
 		} 
 		catch(IOException ex) {
